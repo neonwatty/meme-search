bae27456f1fde07e5e11f748787e52cfb818176b
Jeremy Watt <jermwatt@gmail.com>
2025-03-23 05:58:21 -0700

    clean up

--- meme_search_pro/meme_search_app/app/models/image_to_text.rb
+++
@@ -1,8 +0,0 @@
-class ImageToText < ApplicationRecord
-    validates :name, presence: true
-    validates :resource, presence: true
-    validates :description, presence: true
-    validates :current, inclusion: { in: [ true, false ] }
-    validates_length_of :name, minimum: 1, maximum: 100, allow_blank: false
-    validates_length_of :description, minimum: 1, maximum: 1000, allow_blank: false
-end
